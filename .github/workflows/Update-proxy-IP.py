b8032d485234c25df0eeefd30d87703b5d87a377
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-01-21 10:42:21 +0800

    Update Update-proxy-IP.py

--- .github/workflows/Update-proxy-IP.py
+++ .github/workflows/Update-proxy-IP.py
@@ -82,7 +82,7 @@ try:
     if sha_response.status_code == 200:
         current_sha = sha_response.json().get("sha", "")
         data = {
-            "message": f"Successfully updated ip.txt - {start_time_str} (Total IPs: {len(ip_set)})",
+            "message": f"Updated ip.txt - {start_time_str} (Total IPs: {len(ip_set)})",
             "content": ip_txt_content_base64,
             "sha": current_sha,
         }
