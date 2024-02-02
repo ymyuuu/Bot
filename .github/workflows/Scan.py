f2c1c4dbf8655cee514e79d2e3b92c32498441dc
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-02-03 01:46:27 +0800

    Update Scan.py

--- .github/workflows/Scan.py
+++ .github/workflows/Scan.py
@@ -36,11 +36,11 @@ def clear_files():
 def send_notification(message_text):
     requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", params={'chat_id': chat_id, 'text': message_text, 'parse_mode': 'MarkdownV2'})
 
-def scan_and_send_files(url, filename_prefix):
+def scan_and_send_files(url, filename_prefix, additional_text=None):
     data = requests.get(url).text.strip().split('\n')
     with open(os.path.join(output_path, f"{filename_prefix}.txt"), 'w') as file:
         file.write("\n".join(data))
-    send_to_telegram(os.path.join(output_path, f"{filename_prefix}.txt"), additional_text=f"`{filename_prefix}.txt` is ok")
+    send_to_telegram(os.path.join(output_path, f"{filename_prefix}.txt"), additional_text=additional_text)
 
 proxy_url = "https://ipdb.api.030101.xyz/?type=proxy"
 best_proxy_url = "https://ipdb.api.030101.xyz/?type=bestproxy"
@@ -66,8 +66,8 @@ try:
     for asn in unique_asns:
         send_to_telegram(os.path.join(output_path, f"ASN{asn}.txt"))
 
-    scan_and_send_files(best_proxy_url, "BestProxy")
-    scan_and_send_files(best_cf_url, "BestCF")
+    scan_and_send_files(best_proxy_url, "BestProxy", additional_text="今天天气很好")
+    scan_and_send_files(best_cf_url, "BestCF", additional_text="你好呀")
 
     end_time = datetime.now() + timedelta(hours=8)
     duration = (end_time - start_time).total_seconds()
