8545f051bff8df9f66b9fa6674b6f74299c96f27
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-02-03 02:12:09 +0800

    Update Scan.py

--- .github/workflows/Scan.py
+++ .github/workflows/Scan.py
@@ -20,11 +20,11 @@ def get_ip_info(ip, session, output_path, asn_set):
     except requests.RequestException:
         pass
 
-def send_to_telegram(file_path, additional_text=None):
+def send_to_telegram(file_path, additional_text=None, parse_mode='MarkdownV2'):
     with open(file_path, 'rb') as file:
         files = {'document': (file_path, file, 'rb')}
 
-        data = {'chat_id': chat_id, 'parse_mode': 'HTML'}
+        data = {'chat_id': chat_id, 'parse_mode': parse_mode}
         if additional_text:
             data['caption'] = additional_text
 
@@ -33,14 +33,14 @@ def send_to_telegram(file_path, additional_text=None):
 def clear_files():
     [os.remove(os.path.join(output_path, filename)) for filename in os.listdir(output_path) if filename.startswith(("ASN", "Best")) and filename.endswith(".txt")]
 
-def send_notification(message_text):
-    requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", params={'chat_id': chat_id, 'text': message_text, 'parse_mode': 'HTML'})
+def send_notification(message_text, parse_mode='MarkdownV2'):
+    requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", params={'chat_id': chat_id, 'text': message_text, 'parse_mode': parse_mode})
 
-def scan_and_send_files(url, filename_prefix, additional_text=None):
+def scan_and_send_files(url, filename_prefix, additional_text=None, parse_mode='MarkdownV2'):
     data = requests.get(url).text.strip()
     with open(os.path.join(output_path, f"{filename_prefix}.txt"), 'w') as file:
         file.write(data)
-    send_to_telegram(os.path.join(output_path, f"{filename_prefix}.txt"), additional_text=additional_text)
+    send_to_telegram(os.path.join(output_path, f"{filename_prefix}.txt"), additional_text=additional_text, parse_mode=parse_mode)
 
 proxy_url = "https://ipdb.api.030101.xyz/?type=proxy"
 best_proxy_url = "https://ipdb.api.030101.xyz/?type=bestproxy"
@@ -52,7 +52,7 @@ output_path = os.path.dirname(os.path.realpath(__file__))
 
 try:
     start_time = datetime.now() + timedelta(hours=8)  # Add 8 hours for Beijing time
-    send_notification(f"Scan <b>start</b> at <b>{start_time:%Y-%m-%d %H:%M}</b>")
+    send_notification(f"Scan *start* at **{start_time:%Y-%m-%d %H:%M}**", parse_mode='Markdown')
     print(f"Scan start at {start_time:%Y-%m-%d %H:%M}")
 
     clear_files()
@@ -66,13 +66,13 @@ try:
     for asn in unique_asns:
         send_to_telegram(os.path.join(output_path, f"ASN{asn}.txt"))
 
-    scan_and_send_files(best_proxy_url, "BestProxy", additional_text="`abcd.ooo.cvn`")
-    scan_and_send_files(best_cf_url, "BestCF", additional_text="`bestcfaaa.com`")
+    scan_and_send_files(best_proxy_url, "BestProxy", additional_text="`abcd.ooo.cvn`", parse_mode='MarkdownV2')
+    scan_and_send_files(best_cf_url, "BestCF", additional_text="*bestcfaaa.com*", parse_mode='MarkdownV2')
 
     end_time = datetime.now() + timedelta(hours=8)
     duration = (end_time - start_time).total_seconds()
-    scan_message = f"Scan <b>over</b> at <b>{end_time:%Y-%m-%d %H:%M}</b>\nIPs: {len(proxy_data)}, ASNs: {len(unique_asns)}, Lasted for {duration:.2f}s"
-    send_notification(scan_message)
+    scan_message = f"Scan *over* at **{end_time:%Y-%m-%d %H:%M}**\nIPs: {len(proxy_data)}, ASNs: {len(unique_asns)}, Lasted for {duration:.2f}s"
+    send_notification(scan_message, parse_mode='Markdown')
     print(f"Scan over at {end_time:%Y-%m-%d %H:%M}")
 
 except requests.RequestException:
