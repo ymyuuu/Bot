ae3ba0f18741df14875dd82160451987ad5e6662
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-02-03 01:55:45 +0800

    Update Scan.py

--- .github/workflows/Scan.py
+++ .github/workflows/Scan.py
@@ -24,7 +24,7 @@ def send_to_telegram(file_path, additional_text=None):
     with open(file_path, 'rb') as file:
         files = {'document': (file_path, file, 'rb')}
 
-        data = {'chat_id': chat_id, 'parse_mode': 'MarkdownV2'}
+        data = {'chat_id': chat_id, 'parse_mode': 'Markdown'}
         if additional_text:
             data['caption'] = additional_text
 
@@ -34,7 +34,7 @@ def clear_files():
     [os.remove(os.path.join(output_path, filename)) for filename in os.listdir(output_path) if filename.startswith(("ASN", "Best")) and filename.endswith(".txt")]
 
 def send_notification(message_text):
-    requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", params={'chat_id': chat_id, 'text': message_text, 'parse_mode': 'MarkdownV2'})
+    requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", params={'chat_id': chat_id, 'text': message_text, 'parse_mode': 'Markdown'})
 
 def scan_and_send_files(url, filename_prefix, additional_text=None):
     data = requests.get(url).text.strip()
@@ -52,7 +52,7 @@ output_path = os.path.dirname(os.path.realpath(__file__))
 
 try:
     start_time = datetime.now() + timedelta(hours=8)  # Add 8 hours for Beijing time
-    send_notification(f"Scan start at `{start_time:%Y-%m-%d %H:%M}`")
+    send_notification(f"Scan start at *{start_time:%Y-%m-%d %H:%M}*")
     print(f"Scan start at {start_time:%Y-%m-%d %H:%M}")
 
     clear_files()
@@ -66,12 +66,12 @@ try:
     for asn in unique_asns:
         send_to_telegram(os.path.join(output_path, f"ASN{asn}.txt"))
 
-    scan_and_send_files(best_proxy_url, "BestProxy", additional_text="`abcd.ooo.cvn`")
-    scan_and_send_files(best_cf_url, "BestCF", additional_text="`bestcfaaa.com`")
+    scan_and_send_files(best_proxy_url, "BestProxy", additional_text="abcd.ooo.cvn")
+    scan_and_send_files(best_cf_url, "BestCF", additional_text="bestcfaaa.com")
 
     end_time = datetime.now() + timedelta(hours=8)
     duration = (end_time - start_time).total_seconds()
-    scan_message = f"Scan over at `{end_time:%Y-%m-%d %H:%M}`\nIPs: {len(proxy_data)}, ASNs: {len(unique_asns)}, Lasted for {duration:.2f}s"
+    scan_message = f"Scan over at *{end_time:%Y-%m-%d %H:%M}*\nIPs: {len(proxy_data)}, ASNs: {len(unique_asns)}, Lasted for {duration:.2f}s"
     send_notification(scan_message)
     print(f"Scan over at {end_time:%Y-%m-%d %H:%M}")
 
