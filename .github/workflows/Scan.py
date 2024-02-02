c0b068ce08976de76031bd963c61876b95a979fd
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-02-03 01:25:01 +0800

    Update Scan.py

--- .github/workflows/Scan.py
+++ .github/workflows/Scan.py
@@ -25,13 +25,14 @@ def send_to_telegram(file_path):
         requests.post(f"https://api.telegram.org/bot{bot_token}/sendDocument", files={'document': file}, params={'chat_id': chat_id})
 
 def clear_files():
-    [os.remove(os.path.join(output_path, filename)) for filename in os.listdir(output_path) if filename.startswith("ASN") and filename.endswith(".txt")]
+    [os.remove(os.path.join(output_path, filename)) for filename in os.listdir(output_path) if filename.startswith(("ASN", "Best")) and filename.endswith(".txt")]
 
 def send_notification(message_text):
     requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", params={'chat_id': chat_id, 'text': message_text, 'parse_mode': 'Markdown'})
 
 proxy_url = "https://ipdb.api.030101.xyz/?type=proxy"
 best_proxy_url = "https://ipdb.api.030101.xyz/?type=bestproxy"
+best_cf_url = "https://ipdb.api.030101.xyz/?type=bestcf"
 bot_token = os.environ.get('BOT_TOKEN')  # Read from environment variable
 chat_id = os.environ.get('CHAT_ID')      # Read from environment variable
 
@@ -54,13 +55,19 @@ try:
     with open(os.path.join(output_path, "BestProxy.txt"), 'w') as best_proxy_file:
         best_proxy_file.write("\n".join(best_proxy_data))
 
-    [send_to_telegram(os.path.join(output_path, filename)) for filename in os.listdir(output_path) if filename.startswith("ASN") and filename.endswith(".txt")]
+    best_cf_data = requests.get(best_cf_url).text.strip().split('\n')
+    with open(os.path.join(output_path, "BestCF.txt"), 'w') as best_cf_file:
+        best_cf_file.write("\n".join(best_cf_data))
+        best_cf_file.write("\nBestCF.txt is ok")  # Add the description text
+
+    [send_to_telegram(os.path.join(output_path, filename)) for filename in os.listdir(output_path) if filename.startswith(("ASN", "Best")) and filename.endswith(".txt")]
 
     end_time = datetime.now() + timedelta(hours=8)  # Add 8 hours for Beijing time
     duration = (end_time - start_time).total_seconds()
     send_notification(f"Scan over at *{end_time:%Y-%m-%d %H:%M}*\nIPs: {len(proxy_data)}, ASNs: {len(unique_asns)}, Lasted for {duration:.2f}s")
     print(f"Scan over at {end_time:%Y-%m-%d %H:%M}")
     send_to_telegram(os.path.join(output_path, "BestProxy.txt"))
+    send_to_telegram(os.path.join(output_path, "BestCF.txt"))
 
 except requests.RequestException:
     pass
